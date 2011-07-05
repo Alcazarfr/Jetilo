bb1ed837b132d2f54ba8ccd427b3b04e6d8426f3
_Duncan_Idaho_ <edouard.paumier@gmail.com>
2011-07-05 19:14:15 +0200

    closes #9 Support des messages de niveau 0, envoyé à toute nation sauf celles ayant des agents sur place. Ajout des fichiers manquants au commit précédent

--- index.php
+++ index.php
@@ -22,8 +22,8 @@ include 'includes/prepartie.php';
 
 ?>
 
-<html>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"> <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"> 
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
 <head>
 	<script type="text/javascript" src="./javascripts/jquery.js"></script>
 	<script type="text/javascript" src="./javascripts/jquery.jgrowl.js"></script>
